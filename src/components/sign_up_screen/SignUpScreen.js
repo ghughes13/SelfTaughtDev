8524d0ea6a5d1861b078bddfbb7add891ed99c1c
ghughes13 <guitarguy13@ymail.com>
2020-09-10 17:39:12 -0500

    moved function

--- src/components/sign_up_screen/SignUpScreen.js
+++ src/components/sign_up_screen/SignUpScreen.js
@@ -4,98 +4,100 @@ import axios from "axios"
 import "./sign-up-screen.scss"
 import Loader from "../loader/Loader"
 
-const submitForm = () => {
-  axios
-    .post(
-      "https://www.selftaught-dev.com/.netlify/functions/newUser",
-      JSON.stringify({
-        userName: document.getElementById("userName").value,
-        password: document.getElementById("password").value,
-        firstName: document.getElementById("firstName").value,
-        lastName: document.getElementById("lastName").value,
-        email: document.getElementById("email").value,
-      })
-    )
-    .then(response => {
-      if (response.status === 200) {
-        console.log("IT WORKED MAYBE?")
-      }
-    })
-    .catch(error => {
-      console.log("FAILED ):")
-    })
-}
-
 const SignUpScreen = ({
   pageTitle,
   formTitle,
   urlToPostTo,
   btnText,
   successMessage,
-}) => (
-  <div className="mailing-list-signup">
-    <img
-      className="logo"
-      src={require("../../svgs/selftaughtdev-logo-mini.svg")}
-      alt="logo"
-    />
-    <h1>Sign Up</h1>
-    <form
-      id={formTitle}
-      name={formTitle}
-      onSubmit={e => {
-        submitForm()
-        e.preventDefault()
-      }}
-    >
-      <div className="form-info-div">
-        <label htmlFor="name">First Name</label>
-        <input
-          type="text"
-          placeholder="First Name"
-          name="firstName"
-          id="firstName"
-        />
-        <label htmlFor="name">Last Name</label>
-        <input
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-          id="lastName"
-        />
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" placeholder="Email" id="email" />
-        <label htmlFor="name">Username</label>
-        <input
-          type="text"
-          placeholder="Username"
-          name="username"
-          id="username"
-        />
-        <label htmlFor="name">Password</label>
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          id="password"
-        />
-        <button id="sbmt-form-btn" type="submit">
-          Login
-        </button>
-      </div>
-      <div id="thanks">
-        <p>
-          This shouldn't show up. You should just be taken to the project
-          archive main screen
+}) => {
+  const submitForm = () => {
+    axios
+      .post(
+        "https://www.selftaught-dev.com/.netlify/functions/newUser",
+        JSON.stringify({
+          userName: document.getElementById("userName").value,
+          password: document.getElementById("password").value,
+          firstName: document.getElementById("firstName").value,
+          lastName: document.getElementById("lastName").value,
+          email: document.getElementById("email").value,
+        })
+      )
+      .then(response => {
+        if (response.status === 200) {
+          console.log("IT WORKED MAYBE?")
+        }
+      })
+      .catch(error => {
+        console.log("FAILED ):")
+      })
+  }
+
+  return (
+    <div className="mailing-list-signup">
+      <img
+        className="logo"
+        src={require("../../svgs/selftaughtdev-logo-mini.svg")}
+        alt="logo"
+      />
+      <h1>Sign Up</h1>
+      <form
+        id={formTitle}
+        name={formTitle}
+        onSubmit={e => {
+          submitForm()
+          e.preventDefault()
+        }}
+      >
+        <div className="form-info-div">
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+            id="firstName"
+          />
+          <label htmlFor="name">Last Name</label>
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            id="lastName"
+          />
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="Email" id="email" />
+          <label htmlFor="name">Username</label>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            id="username"
+          />
+          <label htmlFor="name">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            id="password"
+          />
+          <button id="sbmt-form-btn" type="submit">
+            Login
+          </button>
+        </div>
+        <div id="thanks">
+          <p>
+            This shouldn't show up. You should just be taken to the project
+            archive main screen
+          </p>
+        </div>
+        <Loader />
+        <p id="error-msg">
+          Error submitting form. <br />
+          Ensure all fields are filled out.
         </p>
-      </div>
-      <Loader />
-      <p id="error-msg">
-        Error submitting form. <br />
-        Ensure all fields are filled out.
-      </p>
-    </form>
-  </div>
-)
+      </form>
+    </div>
+  )
+}
 
 export default SignUpScreen
