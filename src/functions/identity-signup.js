41ccd225390b2341c20f51c52bcecd50aa262dc5
ghughes13 <guitarguy13@ymail.com>
2020-10-14 14:34:24 -0500

    ident-signup should now post to fuana

--- src/functions/identity-signup.js
+++ src/functions/identity-signup.js
@@ -2,8 +2,36 @@ exports.handler = async (e) => {
   const { user } = JSON.parse(e.body);
   console.log(JSON.stringify(user, null, 2))
 
+  const netlifyID = user.id;
+
+  const stripeID = 1;
+
+  const response = await fetch('https://graphql.fauna.com/graphql', {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+    },
+    body: JSON.stringify({
+      query: `
+      mutation ($netlifyID: ID! $stripeID: ID!) {
+        createUser(data: {netlifyID: $netlifyID, stripeID: $stripeID}) {
+          netlifyID
+          stripeID
+        }
+      }`,
+      variables: {
+        netlifyID,
+        stripeID,
+      }
+    })
+  })
+  .then(res => res.json())
+  .catch((err) => console.error(err))
+
+  console.log({ response })
+
   return {
     statusCode: 200,
-    body: JSON.stringify({app_metadata: { roles: ['sub:free'] }})
+    body: JSON.stringify({app_metadata: { roles: ['Lite'] }})
   }
 }
