1cbfa2161f4a4bb5f850723292cbab3902ac0159
ghughes13 <guitarguy13@ymail.com>
2020-10-20 08:40:30 -0500

    commended out fetch request to make sure that is where the bug is

--- src/functions/identity-signup.js
+++ src/functions/identity-signup.js
@@ -6,31 +6,31 @@ exports.handler = async event => {
   const netlifyID = user.id
   const stripeID = 1
 
-  // TODO create a customer record in Fauna
-  const response = await fetch("https://graphql.fauna.com/graphql", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
-    },
-    body: JSON.stringify({
-      query: `
-        mutation ($netlifyID: ID! $stripeID: ID!) {
-          createUser(data: {netlifyID: $netlifyID, stripeID: $stripeID}) {
-            netlifyID
-            stripeID
-          }
-        }
-      `,
-      variables: {
-        netlifyID,
-        stripeID,
-      },
-    }),
-  })
-    .then(res => res.json())
-    .catch(err => console.error(JSON.stringify(err, null, 2)))
+  // // TODO create a customer record in Fauna
+  // const response = await fetch("https://graphql.fauna.com/graphql", {
+  //   method: "POST",
+  //   headers: {
+  //     Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+  //   },
+  //   body: JSON.stringify({
+  //     query: `
+  //       mutation ($netlifyID: ID! $stripeID: ID!) {
+  //         createUser(data: {netlifyID: $netlifyID, stripeID: $stripeID}) {
+  //           netlifyID
+  //           stripeID
+  //         }
+  //       }
+  //     `,
+  //     variables: {
+  //       netlifyID,
+  //       stripeID,
+  //     },
+  //   }),
+  // })
+  //   .then(res => res.json())
+  //   .catch(err => console.error(JSON.stringify(err, null, 2)))
 
-  console.log({ response })
+  // console.log({ response })
 
   return {
     statusCode: 200,
