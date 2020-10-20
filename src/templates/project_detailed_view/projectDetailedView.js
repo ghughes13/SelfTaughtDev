96b8a0f6b578b5466cd7bcb8993d7c3a476b4910
ghughes13 <guitarguy13@ymail.com>
2020-10-20 17:22:10 -0500

    added useState import

--- src/templates/project_detailed_view/projectDetailedView.js
+++ src/templates/project_detailed_view/projectDetailedView.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../../components/layout/Layout"
 import { useIdentityContext } from "react-netlify-identity-widget"
 import LoginBtn from "../../components/login-btn/LoginBtn"
