b09b4497939443b8263870263c89080e5332535a
ghughes13 <guitarguy13@ymail.com>
2020-12-01 17:14:00 -0600

    testing

--- src/functions/identity-signup.js
+++ src/functions/identity-signup.js
@@ -2,43 +2,43 @@ import fetch from "node-fetch"
 const stripe = require("stripe")(process.env.STRIPE_TEST_SECRET_KEY)
 
 exports.handler = async event => {
-  console.log("hit route")
-  const { user } = JSON.parse(event.body)
+  // console.log("hit route")
+  // const { user } = JSON.parse(event.body)
 
-  const customer = await stripe.customers.create({ email: user.email })
+  // const customer = await stripe.customers.create({ email: user.email })
 
-  await stripe.subscriptions.create({
-    customer: customer.id,
-    items: [{ plan: "price_1HeMItJqkXITmJSI0cxr1KCc" }],
-  })
+  // await stripe.subscriptions.create({
+  //   customer: customer.id,
+  //   items: [{ plan: "price_1HeMItJqkXITmJSI0cxr1KCc" }],
+  // })
 
-  const netlifyID = user.id
-  const stripeID = customer.id
+  // const netlifyID = user.id
+  // const stripeID = customer.id
 
-  const response = await fetch("https://graphql.fauna.com/graphql", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
-    },
-    body: JSON.stringify({
-      query: `
-        mutation ($netlifyID: ID! $stripeID: ID!) {
-          createUser(data: {netlifyID: $netlifyID, stripeID: $stripeID}) {
-            netlifyID
-            stripeID
-          }
-        }
-      `,
-      variables: {
-        netlifyID,
-        stripeID,
-      },
-    }),
-  })
-    .then(res => res.json())
-    .catch(err => console.error(JSON.stringify(err, null, 2)))
+  // const response = await fetch("https://graphql.fauna.com/graphql", {
+  //   method: "POST",
+  //   headers: {
+  //     Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+  //   },
+  //   body: JSON.stringify({
+  //     query: `
+  //       mutation ($netlifyID: ID! $stripeID: ID!) {
+  //         createUser(data: {netlifyID: $netlifyID, stripeID: $stripeID}) {
+  //           netlifyID
+  //           stripeID
+  //         }
+  //       }
+  //     `,
+  //     variables: {
+  //       netlifyID,
+  //       stripeID,
+  //     },
+  //   }),
+  // })
+  //   .then(res => res.json())
+  //   .catch(err => console.error(JSON.stringify(err, null, 2)))
 
-  console.log({ response })
+  // console.log({ response })
 
   return {
     statusCode: 200,
