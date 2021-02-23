9da09c513edc8e278a24d14a2117c1252dd185fe
ghughes13 <battletank130@gmail.com>
2021-02-23 13:04:39 -0600

    modified resume-template

--- src/components/mailing_list_signup/MailingListSignup.js
+++ src/components/mailing_list_signup/MailingListSignup.js
@@ -1,99 +1,101 @@
-import React from "react"
+import React, { useState } from "react"
+import { useForm } from "react-hook-form"
 
 import "./mailing-list-signup.scss"
 import Loader from "../animations/loader/Loader"
 
-const mailingListSignup = ({
+const MailingListSignup = ({
   pageTitle,
   formTitle,
   urlToPostTo,
   btnText,
   successMessage,
-}) => (
-  <div className="mailing-list-signup">
-    <h1>{pageTitle}</h1>
-    <form
-      id={formTitle}
-      method="POST"
-      encType="multipart/form-data"
-      name={formTitle}
-      action={urlToPostTo}
-      onSubmit={e => {
-        e.preventDefault()
-        const submitButton = document.getElementById("sbmt-form-btn")
-        const loader = document.querySelector(".loader")
-        const formName = document.getElementById(formTitle)
+}) => {
+  const { register, handleSubmit, errors } = useForm()
+  const [formError, setFormError] = useState(false)
+  const [signingUp, setSigningUp] = useState(false)
 
-        loader.style.display = "block"
-        submitButton.style.display = "none"
+  const onSubmit = async data => {
+    setSigningUp(true)
+    setFormError(false)
 
-        fetch(formName.getAttribute("action"), {
-          method: "POST",
-          body: new FormData(document.getElementById(formTitle)),
-        })
-          .then(res => {
-            if (res.status === 200) {
-              document.querySelector(".form-info-div").style.display = "none"
-              document.getElementById("thanks").style.display = "flex"
-              loader.style.display = "none"
-            } else {
-              loader.style.display = "none"
-              document.getElementById("error-msg").style.display = "block"
-              submitButton.style.display = "block"
-            }
-          })
-          .catch(error => {
-            loader.style.display = "none"
-            document.getElementById("error-msg").style.display = "block"
-            submitButton.style.display = "block"
-          })
-      }}
-      netlify="true"
-      netlify-honeypot="bot-field"
-    >
-      <div className="form-info-div">
-        <input type="hidden" name="form-name" value={formTitle} />
-        <input type="hidden" name="bot-field" id="bot" />
-        <label htmlFor="name">
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            id="name"
-            required
-          />
-        </label>
-        <label htmlFor="email">
-          <input
-            className="margin-top-input"
-            type="email"
-            name="email"
-            placeholder="Email"
-            id="email"
-            required
-          />
-        </label>
-        <div className="optin-box">
-          <label htmlFor="optin" id="optin-label">
-            <input type="checkbox" name="optin" id="optin" required />I
-            acknowledge that I am also signing up for the SelfTaughtDev free
-            email newsletter.{" "}
+    const formName = document.getElementById(formTitle)
+
+    fetch(formName.getAttribute("action"), {
+      method: "POST",
+      body: new FormData(document.getElementById(formTitle)),
+    })
+      .then(res => {
+        if (res.status === 200) {
+          document.querySelector(".form-info-div").style.display = "none"
+          document.getElementById("thanks").style.display = "flex"
+        }
+      })
+      .catch(e => {
+        setFormError(e.message)
+      })
+  }
+
+  return (
+    <div className="mailing-list-signup">
+      <h1>{pageTitle}</h1>
+      <form
+        id={formTitle}
+        method="POST"
+        encType="multipart/form-data"
+        name={formTitle}
+        action={urlToPostTo}
+        onSubmit={handleSubmit(onSubmit)}
+        netlify="true"
+        netlify-honeypot="bot-field"
+      >
+        <div className="form-info-div">
+          <input type="hidden" name="form-name" value={formTitle} />
+          <input type="hidden" name="bot-field" id="bot" />
+          <label htmlFor="name">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              id="name"
+              required
+            />
           </label>
+          <label htmlFor="email">
+            <input
+              className="margin-top-input"
+              type="email"
+              name="email"
+              placeholder="Email"
+              id="email"
+              required
+            />
+          </label>
+          <div className="optin-box">
+            <label htmlFor="optin" id="optin-label">
+              <input type="checkbox" name="optin" id="optin" required />I
+              acknowledge that I am also signing up for the SelfTaughtDev free
+              email newsletter.{" "}
+            </label>
+          </div>
+          {signingUp ? (
+            <Loader />
+          ) : (
+            <button id="sbmt-form-btn" type="submit">
+              {btnText}
+            </button>
+          )}
         </div>
-        <button id="sbmt-form-btn" type="submit">
-          {btnText}
-        </button>
-      </div>
-      <div id="thanks">
-        <p>{successMessage}</p>
-      </div>
-      <Loader />
-      <p id="error-msg">
-        Error submitting form. <br />
-        Ensure all fields are filled out.
-      </p>
-    </form>
-  </div>
-)
+        <div id="thanks">
+          <p>{successMessage}</p>
+        </div>
+        <p id="error-msg">
+          Error submitting form. <br />
+          Ensure all fields are filled out.
+        </p>
+      </form>
+    </div>
+  )
+}
 
-export default mailingListSignup
+export default MailingListSignup
