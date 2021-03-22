973adf183a248527314a4363842a779426f346ef
ghughes13 <battletank130@gmail.com>
2021-03-22 14:00:39 -0500

    Revert "modified config"
    This reverts commit b7b1ef4e4d6482e4fc76b435afe958af69470393.

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-sitemap`,
     {
       resolve: "gatsby-plugin-robots-txt",
