faaf7a138e54162eee87609fe5be9272e850e38a
ghughes13 <battletank130@gmail.com>
2021-07-27 10:46:06 -0500

    a

--- src/functions/handle-webhooks.js
+++ src/functions/handle-webhooks.js
@@ -54,77 +54,78 @@ exports.handler = ({ body, headers }, context) => {
               },
             }
           )
-            .then(res => {
-              res.json()
-            })
+            .then(res => res.json())
             .then(data => {
               console.log("data")
               console.log(data)
-              data.app_metadata.roles
-            })
-            .catch(err => console.error(err))
+              let roles = data.app_metadata.roles
 
-          const { user } = context.clientContext
+              const { user } = context.clientContext
 
-          let newRole
+              let newRole
 
-          if (productID === "prod_J31BiV5A48O0s3") {
-            //Flooble Homepage
-            newRole = "FloobleHomepage"
-          } else if (productID === "prod_J31DcDunyXG7qG") {
-            //JavaScript Clock
-            newRole = "JavaScriptClock"
-          } else if (productID === "prod_J31EHFHuvLiCxe") {
-            //To-Do List App
-            newRole = "To-DoListApp"
-          } else if (productID === "prod_J31EZti9Iv8x5A") {
-            //Stock Quote App
-            newRole = "Stonks"
-          } else if (productID === "prod_J31EYrgJg2dvJf") {
-            //Issue Tracker
-            newRole = "IssueTracker"
-          } else if (productID === "prod_J31FDwc9xbeAEO") {
-            //Sudoku
-            newRole = "Sudoku"
-          } else if (productID === "prod_J31GrQGlLLzHw0") {
-            //Pokedex
-            newRole = "Pokedex"
-          } else if (productID === "prod_J31H33OC5MBsda") {
-            //Memory Game
-            newRole = "MemoryGame"
-          } else if (productID === "prod_J31HUVBgLkIJEX") {
-            //Weather App
-            newRole = "WeatherApp"
-          } else if (productID === "prod_J31IRLfOgay1sd") {
-            //GIF Search Tool
-            newRole = "GifSearchTool"
-          } else if (productID === "prod_J31INdMFMiVMyc") {
-            //Phrase Guessing Game
-            newRole = "PhraseGuessingGame"
-          } else if (productID === "prod_J5zLyjuxivbVGa") {
-            //Pattern Matching Game
-            newRole = "PatternMatchingGame"
-          } else if (productID === "prod_JvP6WyIlOPtnh3") {
-            //Pattern Matching Game
-            newRole = "QuizApp"
-          } else if (productID === "prod_JvPedxmGqF6dy7") {
-            //Pattern Matching Game
-            newRole = "CheckoutForm"
-          }
+              if (productID === "prod_J31BiV5A48O0s3") {
+                //Flooble Homepage
+                newRole = "FloobleHomepage"
+              } else if (productID === "prod_J31DcDunyXG7qG") {
+                //JavaScript Clock
+                newRole = "JavaScriptClock"
+              } else if (productID === "prod_J31EHFHuvLiCxe") {
+                //To-Do List App
+                newRole = "To-DoListApp"
+              } else if (productID === "prod_J31EZti9Iv8x5A") {
+                //Stock Quote App
+                newRole = "Stonks"
+              } else if (productID === "prod_J31EYrgJg2dvJf") {
+                //Issue Tracker
+                newRole = "IssueTracker"
+              } else if (productID === "prod_J31FDwc9xbeAEO") {
+                //Sudoku
+                newRole = "Sudoku"
+              } else if (productID === "prod_J31GrQGlLLzHw0") {
+                //Pokedex
+                newRole = "Pokedex"
+              } else if (productID === "prod_J31H33OC5MBsda") {
+                //Memory Game
+                newRole = "MemoryGame"
+              } else if (productID === "prod_J31HUVBgLkIJEX") {
+                //Weather App
+                newRole = "WeatherApp"
+              } else if (productID === "prod_J31IRLfOgay1sd") {
+                //GIF Search Tool
+                newRole = "GifSearchTool"
+              } else if (productID === "prod_J31INdMFMiVMyc") {
+                //Phrase Guessing Game
+                newRole = "PhraseGuessingGame"
+              } else if (productID === "prod_J5zLyjuxivbVGa") {
+                //Pattern Matching Game
+                newRole = "PatternMatchingGame"
+              } else if (productID === "prod_JvP6WyIlOPtnh3") {
+                //Pattern Matching Game
+                newRole = "QuizApp"
+              } else if (productID === "prod_JvPedxmGqF6dy7") {
+                //Pattern Matching Game
+                newRole = "CheckoutForm"
+              }
 
-          const response = fetch(`${identity.url}/admin/users/${netlifyID}`, {
-            method: "PUT",
-            headers: {
-              Authorization: `Bearer ${identity.token}`,
-            },
-            body: JSON.stringify({
-              app_metadata: {
-                roles: [...userCurrentRoles, newRole],
-              },
-            }),
-          })
-            .then(res => {
-              res.json()
+              const response = fetch(
+                `${identity.url}/admin/users/${netlifyID}`,
+                {
+                  method: "PUT",
+                  headers: {
+                    Authorization: `Bearer ${identity.token}`,
+                  },
+                  body: JSON.stringify({
+                    app_metadata: {
+                      roles: [...roles, newRole],
+                    },
+                  }),
+                }
+              )
+                .then(res => {
+                  res.json()
+                })
+                .catch(err => console.error(err))
             })
             .catch(err => console.error(err))
         })
