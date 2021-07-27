e4406fd61cd824de21e586b413b9ac744d85b246
ghughes13 <battletank130@gmail.com>
2021-07-26 20:26:35 -0500

    trying somthing else

--- src/functions/handle-webhooks.js
+++ src/functions/handle-webhooks.js
@@ -60,24 +60,6 @@ exports.handler = async ({ body, headers }, context) => {
         newRole = "CheckoutForm"
       }
 
-      const faunaFetch = ({ query, variables }) => {
-        await fetch("https://graphql.fauna.com/graphql", {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
-          },
-          body: JSON.stringify({
-            query,
-            variables,
-          }),
-        })
-          .then(res => {
-            console.log(res.json())
-            res.json()
-          })
-          .catch(err => console.error(JSON.stringify(err, null, 2)))
-      }
-
       const query = `
         query ($stripeID: ID!) {
           getUserByStripeID(stripeID: $stripeID){
@@ -87,7 +69,21 @@ exports.handler = async ({ body, headers }, context) => {
       `
       const variables = { stripeID }
 
-      const result = faunaFetch({ query, variables })
+      const result = await fetch("https://graphql.fauna.com/graphql", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+        },
+        body: JSON.stringify({
+          query,
+          variables,
+        }),
+      })
+        .then(res => {
+          console.log(res.json())
+          res.json()
+        })
+        .catch(err => console.error(JSON.stringify(err, null, 2)))
 
       const netlifyID = result.data.getUserByStripeID.netlifyID
 
