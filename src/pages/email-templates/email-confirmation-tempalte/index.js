59286cad320fb7083642d32ecd2d0c8e86b77003
ghughes13 <battletank130@gmail.com>
2021-08-13 09:26:27 -0500

    changed route on email auth

--- src/pages/email-templates/email-confirmation-tempalte/index.js
+++ src/pages/email-templates/email-confirmation-tempalte/index.js
@@ -5,7 +5,7 @@ const EmailConfirmationTemplate = () => (
     <h2>Confirm your signup</h2>
     <p>Follow this link to confirm your user:</p>
     <p>
-      <a href="{{ .SiteURL }}/password-reset/#confirmation_token={{ .Token }}">
+      <a href="{{ .SiteURL }}/new-user/#confirmation_token={{ .Token }}">
         Confirm your mail
       </a>
     </p>
